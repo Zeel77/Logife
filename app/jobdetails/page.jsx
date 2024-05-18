c1a2883007f5902a12c5da55c1c772460b99c16f
Zeel77 <90300770+Zeel77@users.noreply.github.com>
2024-05-17 22:24:36 -0700

    Update page.jsx

--- app/jobdetails/page.jsx
+++ app/jobdetails/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import {React,useState,useEffect} from 'react'
 import AssignmentIndOutlinedIcon from '@mui/icons-material/AssignmentIndOutlined';
 import OpenWithOutlinedIcon from '@mui/icons-material/OpenWithOutlined';
